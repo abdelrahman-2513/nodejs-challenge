48020a53436dd5405a0df4e016c3c8b1221f6734
Abdelrahman Hassan <ab19622001@gmail.com>
2025-02-04 00:32:31 +0200

    Update statsRoutes.ts

--- src/routes/statsRoutes.ts
+++ src/routes/statsRoutes.ts
@@ -1,10 +1,10 @@
 import { Router } from 'express';
-import { getInactiveUsers, getStatsByUserId, getTopLoginUsers } from '../controllers/statsController';
+import { getInactiveUsers, getTopLoginUsers } from '../controllers/statsController';
 import { authenticateToken, isAdmin } from '../middlewares/authMiddleware';
 import { asyncHandler } from '../utils/asyncHandler';
 
 export const statsRouter = Router();
 
-statsRouter.get('/user/:userId', authenticateToken, asyncHandler(getStatsByUserId));
+
 statsRouter.get('/top', authenticateToken,isAdmin, asyncHandler(getTopLoginUsers));
 statsRouter.get('/inactive', authenticateToken,isAdmin, asyncHandler(getInactiveUsers));
